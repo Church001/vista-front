d064a102a8266c50114b9168e28950c2fcbbdb4e
Remilekun Salami <remilekun.salami@gmail.com>
2019-07-25 13:06:26 +0100

    fix: contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { Form, Label, FormGroup, Input } from 'reactstrap';
+import { Button, Form, Label, FormGroup, Input } from 'reactstrap';
+import { SocialLinks } from 'components';
+import { ReactComponent as Mail } from 'assets/svg/mail.svg';
+import { ReactComponent as Marker } from 'assets/svg/marker.svg';
+import { ReactComponent as Phone } from 'assets/svg/phone.svg';
 
-export const ContactForm = props => {
+export const ContactForm = () => {
   return (
     <div className='cf'>
       <div className='container'>
         <div className='row'>
-          <div className='col-md-6'>
+          <div className='col-md-6 mb-4 mb-md-0'>
             <h1 className='cf__title text text--xl c-white fw-light'>
               How to contact us for what you need?
             </h1>
 
             <div className='cf__info'>
               <div className='cf__info__item'>
-                <div className='icon'>icon</div>
+                <div className='icon'>
+                  <Marker width={20} />
+                </div>
 
                 <div className='texts'>
                   <p className='text c-white mb-0'>
@@ -23,7 +29,9 @@ export const ContactForm = props => {
               </div>
 
               <div className='cf__info__item'>
-                <div className='icon'>icon</div>
+                <div className='icon'>
+                  <Mail width={20} height={20} />
+                </div>
 
                 <div className='texts'>
                   <a className='text c-white' href='mailto:sales@pts.com'>
@@ -39,7 +47,9 @@ export const ContactForm = props => {
               </div>
 
               <div className='cf__info__item'>
-                <div className='icon'>icon</div>
+                <div className='icon'>
+                  <Phone width={20} height={20} />
+                </div>
 
                 <div className='texts'>
                   <p className='text c-white mb-0'>Sales : (081) 508 844 93</p>
@@ -50,7 +60,7 @@ export const ContactForm = props => {
               </div>
             </div>
 
-            <div className='cf__social'>ig</div>
+            <SocialLinks />
           </div>
           <div className='col-md-6'>
             <div className='card cf__card'>
@@ -74,6 +84,12 @@ export const ContactForm = props => {
                   <Label className='form__label'>Message</Label>
                   <Input className='form__field' type={'textarea'} />
                 </FormGroup>
+
+                <div className='d-flex justify-content-center'>
+                  <Button className='btn__purple btn--rounded btn--xl'>
+                    SEND MESSAGE
+                  </Button>
+                </div>
               </Form>
             </div>
           </div>
