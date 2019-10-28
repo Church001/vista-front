95c42c743a3f6dfc93b737e05d6175910d344d89
church <simonreuben23@gmail.com>
2019-10-28 18:38:17 +0100

    added the changes requeste, changung loader for both map and general spinner

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-const Loader = () => (
-  <div className='section section--wo section--wo--p'>
-    <div className='container-fluid'>
-      <div className='section__sub'>
-        <div className='section__header'>loading...</div>
-      </div>
-    </div>
-  </div>
-);
+// const Loader = () => (
+//   <div className='section section--wo section--wo--p'>
+//     <div className='container-fluid'>
+//       <div className='section__sub'>
+//         <div className='section__header'>loading...</div>
+//       </div>
+//     </div>
+//   </div>
+// );
+
+const Loader = () => <div>loading...</div>;
 
 export default Loader;
