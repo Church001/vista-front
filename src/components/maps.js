f426ef05c4db82ed9dceab5a8b4e414bd65c2296
church <simonreuben23@gmail.com>
2019-10-31 10:31:32 +0100

    added the entire features required from the map component

--- src/components/maps.js
+++ src/components/maps.js
@@ -1,19 +1,77 @@
 import React from 'react';
 import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
+import LOGO from '../assets/img/logo.png';
 
 export class Maps extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       showInfoWindow: false,
+      marker: null,
+      markerData: null,
       stores: [
-        { latitude: 9.037831, longitude: 7.383967, name: 'abuja' }, //Abuja
-        { latitude: 6.537404, longitude: 3.332335 }, //Lagos
-        { latitude: 4.815417, longitude: 7.049736 }, //PH
-        { latitude: 7.361452, longitude: 3.869656 }, //Ibadan
-        { latitude: 11.84588, longitude: 13.143745 }, //Maiduguri
-        { latitude: 12.014434, longitude: 8.572272 }, //Kano
-        { latitude: 10.444833, longitude: 7.400554 } //Kaduna
+        {
+          latitude: 9.037831,
+          longitude: 7.383967,
+          name: 'Abuja',
+          title: 'Our Abuja office',
+          phone: '+2349053807969',
+          address:
+            'Plot 764 Cad Zone, C-16, Idu Industrial Area, Near Larfarge Plant Abuja.'
+        }, //Abuja
+        {
+          latitude: 6.537404,
+          longitude: 3.332335,
+          name: 'Lagos',
+          title: 'Our Lagos Office',
+          phone: '+2348169110000',
+          address:
+            '2EB, Opposite Aswani Market, Osolo Way, Aswani-Oshodi Industrial Scheme, Isolo, Lagos.'
+        }, //Lagos
+        {
+          latitude: 4.815417,
+          longitude: 7.049736,
+          name: 'PortHarcourt',
+          title: 'Our PortHarcourt Office',
+          phone: '+2348126300427',
+          address:
+            '270, Trans Amadi Industrial Layout, Triana Ltd Compound Near LG Shop, Opp Mainstreet Bank, PHC.'
+        }, //PH
+        {
+          latitude: 7.361452,
+          longitude: 3.869656,
+          name: 'Ibadan',
+          title: 'Our Ibadan Office',
+          phone: '+2348126300108',
+          address:
+            '8, Ajia Street, Behind Capital Building, Off Ring Road, Ibadan.'
+        }, //Ibadan
+        {
+          latitude: 11.84588,
+          longitude: 13.143745,
+          name: 'Maiduguri',
+          title: 'Our Maiduguri Office',
+          phone: '+2349053807969',
+          address:
+            'Former Leventis Supermarket, Opp Ramat Shopping Plaza, Maiduguri, Borno'
+        }, //Maiduguri
+        {
+          latitude: 12.014434,
+          longitude: 8.572272,
+          name: 'Kano',
+          title: 'Our Kano Office',
+          phone: '+2348150865735',
+          address: 'Kundial Road, Bompal Industrial Layout, Bompal, Kano.'
+        }, //Kano
+        {
+          latitude: 10.444833,
+          longitude: 7.400554,
+          name: 'Kaduna',
+          title: 'Our Kaduna Office',
+          phone: '+2348126300272',
+          address:
+            'I5 Inuwa Abdulkadir Road, Industrial Estate, Kaduna South, Kaduna.'
+        } //Kaduna
       ]
     };
   }
@@ -21,13 +79,25 @@ export class Maps extends React.Component {
   onMapClicked = () => {
     if (this.state.showInfoWindow) {
       this.setState({
-        showInfoWindow: false
+        showInfoWindow: false,
+        marker: null
       });
     }
   };
 
-  markerOnClick = (props, marker, e) => {
-    const data = { props, marker, e };
+  markerOnClick = (props, marker) => {
+    const data = { props, marker };
+    const markerData = {
+      position: props.position,
+      name: marker.name,
+      address: props.address,
+      phone: props.phone
+    };
+    this.setState({
+      marker: marker,
+      showInfoWindow: marker ? true : false,
+      markerData: markerData
+    });
     console.log(data);
   };
 
@@ -38,6 +108,9 @@ export class Maps extends React.Component {
           onClick={this.markerOnClick}
           key={store.latitude}
           name={store.name}
+          title={store.title}
+          phone={store.phone}
+          address={store.address}
           position={{
             lat: store.latitude,
             lng: store.longitude
@@ -52,7 +125,10 @@ export class Maps extends React.Component {
       width: '100%',
       height: '100%'
     };
-
+    let place;
+    if (this.state.markerData !== null) {
+      place = this.state.markerData;
+    }
     return (
       <Map
         google={this.props.google}
@@ -62,12 +138,41 @@ export class Maps extends React.Component {
         onClick={this.onMapClicked}
       >
         {this.variousStores()}
+        <InfoWindow
+          visible={this.state.showInfoWindow}
+          marker={this.state.marker}
+          style={{ maxWidth: 500 }}
+        >
+          <div>
+            <div
+              style={{
+                width: 250,
+                height: 100
+              }}
+            >
+              <img
+                style={{
+                  width: '100%',
+                  height: '100%'
+                }}
+                src={LOGO}
+                alt={place ? place.name : ''}
+              />
+            </div>
+            <hr />
+            <h1>{place ? place.name : ''}</h1>
+            <span>
+              <p>{place ? place.address : ''}</p>
+            </span>
+            <h3>TEL: {place ? place.phone : ''}</h3>
+          </div>
+        </InfoWindow>
       </Map>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apikey: 'AIzaSyDKjYADwaHwaCBY1CVVdJlcef_6rJAmb6I',
+  apiKey: 'AIzaSyDKjYADwaHwaCBY1CVVdJlcef_6rJAmb6I',
   LoadingContainer: () => <div>{''}</div>
 })(Maps);
