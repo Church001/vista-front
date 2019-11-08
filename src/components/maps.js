7d09d300804b77d3e56767f3b560e32c78850d99
church <simonreuben23@gmail.com>
2019-11-08 16:07:48 +0100

    added more stylings to the info window on the map component

--- src/components/maps.js
+++ src/components/maps.js
@@ -199,12 +199,24 @@ export class Maps extends React.Component {
               <p>{place ? place.address : ''}</p>
             </span>
             <hr />
-            <div>
-              <h3>TEL: {place ? place.phone : ''}</h3>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column'
+              }}
+            >
               <div>
+                <h3>TEL: {place ? place.phone : ''}</h3>
+              </div>
+              <div style={{ justifyContent: 'flex-end' }}>
                 {this.state.isMarkerClicked ? (
                   <Badge>
                     <a
+                      style={{
+                        color: 'green',
+                        textDecoration: 'none',
+                        backgroundColor: 'white'
+                      }}
                       href={`https://www.google.com/maps/place/${place.name}/@${place.position.lat},${place.position.lng},18.7z`}
                       target='blank'
                     >
